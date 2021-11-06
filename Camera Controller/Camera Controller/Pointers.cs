c4904b43fa4f72797c5f987f9c7a0662b38358f2
ARCHIE <archie.goldmill@gmail.com>
2021-11-06 22:22:41 +0300

    Removed as game is dead

--- Camera Controller/Camera Controller/Pointers.cs
+++ Camera Controller/Camera Controller/Pointers.cs
@@ -45,13 +45,10 @@ namespace CameraController
 
 		public static long[] Rotation;
 
-		// 0F 29 81 A0 00 00 00 0F 28 48 10 0F 29 89 B0 00 00 00 0F 28 40 20 0F 29 81 C0 00 00 00 0F 28 48 30 0F 29 89 D0 00 00 00
 		public static long CamCodeCave = 0x14DBC2737;
 
-		// F3 0F 11 87 68 11 00 00 48 85 DB 74 25 48 8B 03 48 89 D9 FF 50 08 0F 2F C6 77 0B 48 8B 5B 08 48 85 DB 75 E9 EB 0C F3 0F
 		public static long FovCodePtr = 0x149A5DF23;
 
-		// 0B BE 75 3F C7 83 18 01 00 00 CD CC 4C 3D C7 83 1C 01 00 00 00 40 1C 46 C7 83 20 01 00 00 AB AA AA 3F C7 83 24 01 00 00
 		public static long LOD = 0x14D88734C;
 
 		public static long AllPartsOptional = 0x141D4BCC1;
